40605c6d3e055b3f95bd4bdc9bc89fe4701ce4ad
Abdul Rahim <abdulrahimshoid@gmail.com>
2023-03-13 23:12:42 +0000

    add(karasu): routes folder

---
+++ src/routes/main.ts
@@ -0,0 +1,18 @@
+import { ApiRequest, ApiResponse, methods, Route } from '@sapphire/plugin-api';
+
+export class UserRoute extends Route {
+	public constructor(context: Route.Context, options: Route.Options) {
+		super(context, {
+			...options,
+			route: ''
+		});
+	}
+
+	public [methods.GET](_request: ApiRequest, response: ApiResponse) {
+		response.json({ message: 'Landing Page!' });
+	}
+
+	public [methods.POST](_request: ApiRequest, response: ApiResponse) {
+		response.json({ message: 'Landing Page!' });
+	}
+}
